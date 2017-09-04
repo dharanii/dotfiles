49558d4386e3acc7878b7718f92ca228594ddc44
dharanii <c1h41241v11@gmail.com>
2017-09-04 19:29:13 +0000

    Update

--- lib/xmonad-stack/src/Main.hs
+++ lib/xmonad-stack/src/Main.hs
@@ -10,26 +10,75 @@ import System.Exit (ExitCode(..))
 import System.FilePath ((</>))
 import XMonad
 import XMonad.Config.Desktop (desktopConfig)
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, PP(..), wrap, pad, shorten)
+import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks)
+import XMonad.Layout.Gaps (gaps, Direction2D(..))
 import XMonad.Layout.Spacing (spacingWithEdge)
 import XMonad.Util.Cursor (setDefaultCursor, xC_left_ptr)
 import XMonad.Util.EntryHelper (withCustomHelper, Config(..), withLock, compileUsingShell)
 import qualified XMonad.Util.EntryHelper as EH
+import XMonad.Util.Loggers (date)
+import XMonad.Util.Run (spawnPipe, hPutStrLn)
 
+
+barFont = "-*-lemon-*"
+barIcon = "-*-siji-*"
+barHeight = 36
+
+logHook_ h color3 color8 = dynamicLogWithPP $ def
+    { ppCurrent         = pad
+    , ppHidden          = wrap ("%{F" ++ maybe "#F0F0F0" T.unpack color3 ++ "}") "%{F}" . pad
+    , ppHiddenNoWindows = wrap ("%{F" ++ maybe "#F0F0F0" T.unpack color8 ++ "}") "%{F}" . pad
+    , ppSep = mempty 
+    , ppWsSep = " "
+    , ppLayout = \name ->
+        maybe name T.unpack $ T.stripPrefix "SpacingWithEdge 9 " $ T.pack name 
+    , ppOrder = \(workspaces:layout:tile:[date]) -> 
+        let
+            icon x =
+                "%{F" ++ maybe "#F0F0F0" T.unpack color3 ++ "}" ++ x ++ "%{F}"
+        in
+            [ "%{l}   " ++ workspaces 
+                ++ "  |  " 
+                ++ layout 
+                ++ "  |  " 
+                ++ icon "\57529" ++ " " ++ tile
+            , "%{c}" ++ date 
+            ]
+    , ppExtras = [ date "%a %d %b - %l:%M %p" ]
+    , ppOutput = hPutStrLn h
+    }
 
 main :: IO ()
 main = do
     homeDir <- getHomeDirectory
     colorsJson <- readFile $ homeDir </> ".cache/wal/colors.json"
-    let color0 = colorsJson ^? key "colors" . key "color0" . _String
-        color1 = colorsJson ^? key "colors" . key "color1" . _String
+    let color0  = colorsJson ^? key "colors" . key "color0" . _String
+        color1  = colorsJson ^? key "colors" . key "color1" . _String
+        color3  = colorsJson ^? key "colors" . key "color3" . _String
+        color8  = colorsJson ^? key "colors" . key "color8" . _String
+        color15 = colorsJson ^? key "colors" . key "color15" . _String
+
+    lemonbar <- spawnPipe $
+        "lemonbar"
+            ++ " -d"
+            ++ " -g x" ++ show barHeight 
+            ++ " -B \"" ++ maybe "#000000" T.unpack color0  ++ "\"" 
+            ++ " -F \"" ++ maybe "#F0F0F0" T.unpack color15 ++ "\"" 
+            ++ " -n \"bar\""
+            ++ " -f \"" ++ barFont ++ "\""
+            ++ " -f \"" ++ barIcon ++ "\""
+            ++ " | bash"
 
     withCustomHelper $ EH.defaultConfig
         { run = xmonad $ def
-            { layoutHook = spacingWithEdge 9 $ layoutHook def
+            { layoutHook = gaps [(U, barHeight)] $ spacingWithEdge 9 $ layoutHook def
             , terminal = "urxvtc"
             , normalBorderColor = maybe def T.unpack color0
             , focusedBorderColor = maybe def T.unpack color1
             , startupHook = setDefaultCursor xC_left_ptr <+> startupHook def
+            , logHook = logHook_ lemonbar color3 color8
+            --, manageHook = manageDocks <+> manageHook def
             }
         , compile = compile_ 
         , postCompile = postCompile_
