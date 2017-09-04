430faee1384d86e9a3e01b1281bd3bfaa0453c00
dharanii <c1h41241v11@gmail.com>
2017-09-04 15:06:02 +0000

    Update

--- lib/xmonad-stack/src/Main.hs
+++ lib/xmonad-stack/src/Main.hs
@@ -5,17 +5,19 @@ import Control.Lens ((^?))
 import Control.Monad (void)
 import Data.Aeson.Lens (key, _String)
 import Data.Text as T
+import System.Directory (getHomeDirectory)
 import System.Exit (ExitCode(..))
+import System.FilePath ((</>))
 import XMonad
 import XMonad.Config.Desktop (desktopConfig)
 import XMonad.Util.EntryHelper (withCustomHelper, Config(..), withLock, compileUsingShell)
 import qualified XMonad.Util.EntryHelper as EH
 
 
-
 main :: IO ()
 main = do
-    colorsJson <- readFile "/home/vagrant/.cache/wal/colors.json"
+    homeDir <- getHomeDirectory
+    colorsJson <- readFile $ homeDir </> ".cache/wal/colors.json"
     let color0 = colorsJson ^? key "colors" . key "color0" . _String
         color1 = colorsJson ^? key "colors" . key "color1" . _String
 
@@ -34,11 +36,11 @@ postCompile_ :: ExitCode -> IO ()
 
 compile_ force = 
     withLock ExitSuccess $ compileUsingShell $
-        "cd /home/vagrant/dotfiles/lib/xmonad-stack; " ++
+        "cd \"${HOME}/dotfiles/lib/xmonad-stack\"; " ++
             if force then
                 "stack clean; stack build"
             else
                 "stack build"
 
-postCompile_ ExitSuccess = void $ compileUsingShell "cd /home/vagrant/dotfiles/lib/xmonad-stack; stack install"
+postCompile_ ExitSuccess = void $ compileUsingShell "cd \"${HOME}/dotfiles/lib/xmonad-stack\"; stack install"
 postCompile_ exitCode    = EH.defaultPostCompile exitCode
